22898a339091c56a0230de9548c73fe49ae7d41d
Gina Lin <ginalin@alum.ccu.edu.tw>
2021-10-30 14:00:38 +0800

    create task app

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,34 @@
+from django import forms
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
+# global variable that entire application can access to.
+tasks = []
+
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
+def add(request):
+    if request.method == "POST":
+        # request.POST contains all data that user submitted 
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            # take data from the form
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            # return exsiting data form and promopt error message to user
+            return render(request,"tasks/add.html",{
+                "form":form
+            })
+    # if user only want to get the page, return a empty form
+    return render(request, "tasks/add.html", {
+        "form": NewTaskForm()
+    })
